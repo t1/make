8eeb21d1383d39a60df807c79a87405f0db95de2
Rüdiger zu Dohna <snackbox@sinntr.eu>
2014-08-12 08:36:39 +0200

    test merging of features

--- src/test/java/com/github/t1/sMake/XmlStoredProductTest.java
+++ src/test/java/com/github/t1/sMake/XmlStoredProductTest.java
@@ -8,10 +8,10 @@ import java.nio.file.Paths;
 import org.junit.*;
 
 public class XmlStoredProductTest extends AbstractTest {
-    private static final Id LOMBOK = dependency("org.projectlombok:lombok");
-    private static final Version VERSION = LOMBOK.version("1.12.6");
-    private static final String NAME = "Project Lombok";
-    private static final String DESCRIPTION = "Simplify your code";
+    private static final Id LOMBOK_ID = dependency("org.projectlombok:lombok");
+    private static final Version LOMBOK_VERSION = LOMBOK_ID.version("1.12.6");
+    private static final String LOMBOK_NAME = "Project Lombok";
+    private static final String LOMBOK_DESCRIPTION = "Simplify your code";
 
     private final FileSystemRepository repository = new FileSystemRepository(Paths.get("src", "test", "resources",
             "repository"));
@@ -26,34 +26,45 @@ public class XmlStoredProductTest extends AbstractTest {
         Repositories.getInstance().deregister(repository);
     }
 
+    private void assertJunitHamcrestMockito(Product jhm) {
+        Product junit = jhm.get(dependency("junit:junit"));
+        assertEquals("4.11", junit.version().versionString());
+
+        Product hamcrest = jhm.get(dependency("org.hamcrest:hamcrest-core"));
+        assertEquals("1.2.1", hamcrest.version().versionString());
+
+        Product mockito = jhm.get(dependency("org.mockito:mockito-all"));
+        assertEquals("1.9.5", mockito.version().versionString());
+    }
+
     @Test
     public void shouldReadDependencyFromXmlFile() {
-        Product lombok = Repositories.getInstance().get(VERSION).get();
+        Product lombok = Repositories.getInstance().get(LOMBOK_VERSION).get();
 
-        assertEquals(VERSION, lombok.version());
-        assertEquals(NAME, lombok.name());
-        assertEquals(DESCRIPTION, lombok.description());
+        assertEquals(LOMBOK_VERSION, lombok.version());
+        assertEquals(LOMBOK_NAME, lombok.name());
+        assertEquals(LOMBOK_DESCRIPTION, lombok.description());
         assertEquals(null, lombok.releaseTimestamp());
     }
 
     @Test
     public void shouldResolveDependencyFromFileSystem() {
-        Product product = createProduct().add(newProduct(VERSION));
+        Product product = createProduct().add(newProduct(LOMBOK_VERSION));
 
-        Product lombok = product.get(LOMBOK);
-        assertEquals(VERSION, lombok.version());
-        assertEquals(NAME, lombok.name());
-        assertEquals(DESCRIPTION, lombok.description());
+        Product lombok = product.get(LOMBOK_ID);
+        assertEquals(LOMBOK_VERSION, lombok.version());
+        assertEquals(LOMBOK_NAME, lombok.name());
+        assertEquals(LOMBOK_DESCRIPTION, lombok.description());
     }
 
     @Test
     public void shouldRetainNameWhenResolvingDependencyFromFileSystem() {
-        Product product = createProduct().add(newProduct(VERSION).name("foo"));
+        Product product = createProduct().add(newProduct(LOMBOK_VERSION).name("foo"));
 
-        Product lombok = product.get(LOMBOK);
-        assertEquals(VERSION, lombok.version());
+        Product lombok = product.get(LOMBOK_ID);
+        assertEquals(LOMBOK_VERSION, lombok.version());
         assertEquals("foo", lombok.name());
-        assertEquals(DESCRIPTION, lombok.description());
+        assertEquals(LOMBOK_DESCRIPTION, lombok.description());
     }
 
     @Test
@@ -65,13 +76,24 @@ public class XmlStoredProductTest extends AbstractTest {
 
         Product jhm = product.get(feature("com.github.t1:junit-hamcrest-mockito"));
 
-        Product junit = jhm.get(dependency("junit:junit"));
-        assertEquals("4.11", junit.version().versionString());
+        assertJunitHamcrestMockito(jhm);
+    }
 
-        Product hamcrest = jhm.get(dependency("org.hamcrest:hamcrest-core"));
-        assertEquals("1.2.1", hamcrest.version().versionString());
+    @Test
+    public void shouldMergeDependenciesWhenResolvingFromFileSystem() {
+        Product product = newProduct(product("test:prod"), "1.0") //
+                .name("Test Product").description("A product used for tests") //
+                .add(newProduct(feature("com.github.t1:junit-hamcrest-mockito").version("1.0")) //
+                        .add(newProduct(LOMBOK_VERSION))) //
+        ;
 
-        Product mockito = jhm.get(dependency("org.mockito:mockito-all"));
-        assertEquals("1.9.5", mockito.version().versionString());
+        Product jhm = product.get(feature("com.github.t1:junit-hamcrest-mockito"));
+
+        assertJunitHamcrestMockito(jhm);
+
+        Product lombok = jhm.get(LOMBOK_ID);
+        assertEquals(LOMBOK_VERSION, lombok.version());
+        assertEquals(LOMBOK_NAME, lombok.name());
+        assertEquals(LOMBOK_DESCRIPTION, lombok.description());
     }
 }
