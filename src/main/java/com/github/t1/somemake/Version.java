824fad91182024a777a6d6383a8865016bf9aa6c
Rüdiger zu Dohna <snackbox@sinntr.eu>
2014-08-16 10:28:10 +0200

    improved version comparing

--- src/main/java/com/github/t1/somemake/Version.java
+++ src/main/java/com/github/t1/somemake/Version.java
@@ -7,10 +7,47 @@ import lombok.*;
 
 @Value
 public class Version {
-    private static final Comparator<String> AS_VERSION = new Comparator<String>() {
+    private static final Comparator<String> VERSION = new Comparator<String>() {
         @Override
-        public int compare(String version1, String version2) {
+        public int compare(String thisVersion, String thatVersion) {
+            String[] thisParts = split(thisVersion);
+            String[] thatParts = split(thatVersion);
+            int i;
+            for (i = 0; i < thisParts.length; i++) {
+                String thisPart = thisParts[i];
+                if (thatParts.length < i + 1)
+                    return 1;
+                String thatPart = thatParts[i];
+
+                if (thisPart.equals(thatPart))
+                    continue;
+
+                if (isNumeric(thisPart)) {
+                    if (isNumeric(thatPart)) {
+                        Integer thisInt = Integer.valueOf(thisPart);
+                        Integer thatInt = Integer.valueOf(thatPart);
+                        int c = thisInt.compareTo(thatInt);
+                        if (c == 0)
+                            continue;
+                        return c;
+                    }
+                    return 1; // numbers are always bigger than strings
+                }
+                if (isNumeric(thatPart))
+                    return -1; // strings are always smaller than numbers
+                return thisPart.compareToIgnoreCase(thatPart);
+            }
+            if (thatParts.length > i)
+                return -1;
             return 0;
+        }
+
+        private String[] split(String thisVersion) {
+            return thisVersion.split("(\\.|-)");
+        }
+
+        private boolean isNumeric(String string) {
+            return string.matches("\\d+");
         }
     };
 
@@ -29,7 +66,9 @@ public class Version {
     }
 
     public Optional<String> resolve(Stream<String> versions) {
-        return versions.filter(v -> matches(v)).sorted(AS_VERSION).findFirst();
+        return versions.filter(v -> matches(v)) //
+                // .sorted().peek(v -> System.out.println(v)) //
+                .max(VERSION);
     }
 
     public boolean matches(Version version) {
@@ -37,13 +76,12 @@ public class Version {
     }
 
     public boolean matches(String version) {
-        System.out.println("match " + version + " against " + versionString);
         if (versionString.endsWith("*"))
-            return starMatches(version);
+            return wildcardMatches(version);
         return versionString.equals(version);
     }
 
-    private boolean starMatches(String pattern) {
+    private boolean wildcardMatches(String pattern) {
         int i = versionString.indexOf('*');
         return pattern.substring(0, i).equals(versionString.substring(0, i));
     }
--- src/test/java/com/github/t1/somemake/VersionTest.java
+++ src/test/java/com/github/t1/somemake/VersionTest.java
@@ -1,66 +1,76 @@
 package com.github.t1.somemake;
 
 import static com.github.t1.somemake.Type.*;
-import static java.util.Arrays.*;
 import static org.junit.Assert.*;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
 public class VersionTest {
     private Optional<String> resolve(String pattern, String... matches) {
         Version version = feature("dummy").version(pattern);
-        return version.resolve(asList(matches).stream());
+        return version.resolve(Stream.of(matches));
     }
 
     @Test
     public void shouldResolveSimple() {
-        assertTrue(resolve("1.2.3", "1.2.3").isPresent());
+        assertEquals("1.2.3", resolve("1.2.3", "1.2.3").get());
     }
 
     @Test
     public void shouldNotResolveSimpleWrongVersion() {
-        assertTrue(!resolve("1.2.3", "1.2.4").isPresent());
+        assertFalse(resolve("1.2.3", "1.2.4").isPresent());
     }
 
     @Test
     public void shouldResolveBefore() {
-        assertTrue(resolve("1.2.3", "1.2.3", "1.2.4").isPresent());
+        assertEquals("1.2.3", resolve("1.2.3", "1.2.3", "1.2.4").get());
     }
 
     @Test
     public void shouldResolveAfter() {
-        assertTrue(resolve("1.2.3", "1.2.2", "1.2.3").isPresent());
+        assertEquals("1.2.3", resolve("1.2.3", "1.2.2", "1.2.3").get());
     }
 
     @Test
     public void shouldResolveSingleStar() {
-        assertTrue(resolve("*", "1.2.3").isPresent());
+        assertEquals("1.2.3", resolve("*", "1.2.3").get());
     }
 
     @Test
     public void shouldResolve1Star() {
-        assertTrue(resolve("1*", "1.2.3").isPresent());
+        assertEquals("1.2.3", resolve("1*", "1.2.3").get());
     }
 
     @Test
     public void shouldResolve1DotStar() {
-        assertTrue(resolve("1.*", "1.2.3").isPresent());
+        assertEquals("1.2.3", resolve("1.*", "1.2.3").get());
     }
 
     @Test
     public void shouldNotResolve2Star() {
-        assertTrue(!resolve("2*", "1.2.3").isPresent());
+        assertFalse(resolve("2*", "1.2.3").isPresent());
     }
 
     @Test
     public void shouldNotResolve2DotStar() {
-        assertTrue(!resolve("2.*", "1.2.3").isPresent());
+        assertFalse(resolve("2.*", "1.2.3").isPresent());
     }
 
     @Test
     public void shouldNotResolveStar3() {
-        assertTrue(!resolve("*3", "1.2.3").isPresent());
+        assertFalse(resolve("*3", "1.2.3").isPresent());
+    }
+
+    @Test
+    public void shouldResolveHighest1DotStar() {
+        assertEquals("1.10.3", resolve("1.*", "1.3.1", "1.2.4", "1.3", "1.2.3", "1.10.3", "10.2.3", "2.6.7").get());
+    }
+
+    @Test
+    public void shouldResolveHighest1DotStarWithSnapshot() {
+        assertEquals("1.2.3", resolve("1.*", "1.2.3", "1.2-SNAPSHOT").get());
     }
 }
